4273707902f37baf4c7b89c668ce267374b92812
Anwesh43 <anweshthecool0@gmail.com>
2021-08-08 16:10:14 +0530

    adding LPANode class

--- index.ts
+++ index.ts
@@ -132,3 +132,45 @@ class Animator {
         }
     }
 }
+
+class LPANode {
+
+    prev : LPANode 
+    next : LPANode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LPANode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLPANode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LPANode {
+        var curr : LPANode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
